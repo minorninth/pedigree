5c9193dd9606cc3ae4ae4c18ff7f689e4da2ed06
Dominic Mazzoni <dmazzoni@gmail.com>
2021-07-19 16:37:16 -0700

    Format pedigree-button

--- js/pedigree_button.js
+++ js/pedigree_button.js
@@ -1,7 +1,7 @@
-Vue.component('pedigree-button', {
+Vue.component("pedigree-button", {
   props: {
-      buttontype: String,
-      caption: String,
+    buttontype: String,
+    caption: String,
   },
   template: `
     <button v-on:click="onClick()">
@@ -13,19 +13,19 @@ Vue.component('pedigree-button', {
     </button>
   `,
   computed: {
-      imgClass: function() {
-	  return "img " + this.buttontype;
-      },
-      overlayClass: function() {
-	  return "overlay " + this.buttontype;
-      },
-      img2Class: function() {
-	  return "img2 " + this.buttontype;
-      },
+    imgClass: function () {
+      return "img " + this.buttontype;
+    },
+    overlayClass: function () {
+      return "overlay " + this.buttontype;
+    },
+    img2Class: function () {
+      return "img2 " + this.buttontype;
+    },
   },
   methods: {
-    onClick: function() {
-      this.$emit('click');
-    }
-  }
+    onClick: function () {
+      this.$emit("click");
+    },
+  },
 });
